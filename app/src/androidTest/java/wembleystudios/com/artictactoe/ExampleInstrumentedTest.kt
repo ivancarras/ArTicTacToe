07743c0fb9cfc3a2bc43c254151c26bfbec33db6
Iván Carrasco Alonso <ivan.carrasco@wembleystudios.com>
2018-12-01 22:27:18 +0100

    Building Android project

---
+++ app/src/androidTest/java/wembleystudios/com/artictactoe/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package wembleystudios.com.artictactoe
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("wembleystudios.com.artictactoe", appContext.packageName)
+    }
+}
